16f00d1b493fc429fe91663b07f80bfa3f29d4a5
Bonnie-wy <waiyeewong89@gmail.com>
2022-03-20 00:09:18 +0000

    form and tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,61 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("inputs should be initially empty", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailInputElement = screen.getByRole("textbox");
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+  expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
+});
+
+test("should be able to type an email", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  userEvent.type(emailInputElement, "selena@gmail.com");
+  expect(emailInputElement.value).toBe("selena@gmail.com");
+});
+
+test("should be able to type a password", () => {
+  render(<App />);
+  const passwordInputElement = screen.getByLabelText("Password");
+  userEvent.type(passwordInputElement, "pa$$w0rd");
+  expect(passwordInputElement.value).toBe("pa$$w0rd");
+});
+
+test("should be able to type a confirm password", () => {
+  render(<App />);
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+  userEvent.type(confirmPasswordInputElement, "password!");
+  expect(confirmPasswordInputElement.value).toBe("password!");
+});
+
+test("should show email error message on invalid email", () => {
+  render(<App />);
+  const emailErrorElement = screen.queryByText(
+    /The email you input is invalid/
+  );
+  const emailInputElement = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  const submitBtnElement = screen.getByRole("button", {
+    name: /submit/i,
+  });
+
+  expect(emailErrorElement).not.toBeInTheDocument();
+
+  userEvent.type(emailInputElement, "selenagamil.com");
+  userEvent.click(submitBtnElement);
+
+  const emailErrorElement2 = screen.queryByText(
+    /The email you input is invalid/
+  );
+  expect(emailErrorElement2).toBeInTheDocument();
 });
